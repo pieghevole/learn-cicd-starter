c4ea21ac6859ec45b0371411cb6b7db58bdbf098
Antonio Petrillo <antonio.petrillo4@studenti.unina.it>
2024-12-03 21:09:15 +0100

    add tests

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,54 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestMissingAuthorizationHeaderShouldReturnAnError(t *testing.T) {
+	headers := make(http.Header)
+
+	key, err := GetAPIKey(headers)
+
+	if err == nil {
+		t.Fatalf("Expected error, got nil")
+	}
+
+	if err != ErrNoAuthHeaderIncluded {
+		t.Fatalf("Expected error to be %q, got %q", ErrNoAuthHeaderIncluded.Error(), err.Error())
+	}
+
+	if key != "" {
+		t.Fatalf("Expected key to be empty got %q", key)
+	}
+}
+
+func TestMalformedApiKeyShouldReturnAnError(t *testing.T) {
+	headers := make(http.Header)
+	headers.Set("Authorization", "ApiKex asdfasdf")
+
+	key, err := GetAPIKey(headers)
+
+	if err == nil {
+		t.Fatalf("Expected error, got nil")
+	}
+
+	if key != "" {
+		t.Fatalf("Expected key to be empty got %q", key)
+	}
+}
+
+func TestGetCorrectAuthKey(t *testing.T) {
+	headers := make(http.Header)
+	headers.Set("Authorization", "ApiKey asdfasdf")
+
+	key, err := GetAPIKey(headers)
+
+	if err != nil {
+		t.Fatalf("Unexpected error, got %q", err.Error())
+	}
+
+	if key != "asdfasdfx" {
+		t.Fatalf("Expected key to be %q got %q", "asdfasdf", key)
+	}
+}
